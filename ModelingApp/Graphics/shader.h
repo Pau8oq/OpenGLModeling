a32c710c7e7aded33d50a23a795761b953a06a28
Vasyl Uhryn <ugrinvasil@gmail.com>
2022-06-22 18:46:01 +0300

    draw  square
    create shader

---
+++ ModelingApp/Graphics/shader.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <glad/glad.h>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexShaderPath, const char* fragmentShaderPath);
+
+	void active();
+	void clear();
+
+private:
+	void generate(const char* vertexShaderPath, const char* fragmentShaderPath);
+	GLuint compileShader(const char* shaderPath, GLenum type);
+	std::string loadShader(const char* path);
+};
--- ModelingApp/src/main.cpp
+++ ModelingApp/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include "../Graphics/shader.h"
 
 #include "../IO/screen.h"
 
@@ -8,6 +14,19 @@
 void process_input(GLFWwindow* window);
 
 Screen screen;
+
+float vertices[] = {
+	 0.5f,  0.5f, 0.0f,  
+	 0.5f, -0.5f, 0.0f, 
+	-0.5f, -0.5f, 0.0f,  
+	-0.5f,  0.5f, 0.0f 
+};
+
+unsigned int indices[] =
+{
+	0, 1, 3,
+	1, 2, 3
+};
 
 int main()
 {
@@ -35,7 +54,31 @@ int main()
 	}
 
 	screen.setParameters();
+
+	Shader shader("assets/shaders/vertex.shader", "assets/shaders/fragment.shader");
+
 	
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
 
 	while (!screen.shouldClose())
 	{
@@ -43,10 +86,19 @@ int main()
 
 		//process_input(window);
 
+		shader.active();
+		glBindVertexArray(VAO);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		
 
 		screen.newFrame();
 	}
 
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	shader.clear();
 
 	glfwTerminate();
 	return 0;
